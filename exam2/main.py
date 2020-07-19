2b3ed4e85d068a1ee62d760db827df1e519f8b29
yangming.lin <yangming.lin@ehsn.com.tw>
2020-07-19 17:38:40 +0800

    process train.csv and test.csv

--- exam2/main.py
+++ exam2/main.py
@@ -4,5 +4,13 @@ import pandas as pd
 if __name__ == '__main__':
     if not os.path.exists('result'):
         os.mkdir('result')
+        
+    for data in ['train', 'test']:
+    
+        data_pd = pd.read_csv(f"./data/{data}_need_aggregate.csv")
+        data_pd['datetime'] = data_pd['datetime'].apply(lambda x : x[:-4])
+        data_pd = data_pd.groupby('datetime')['EventId'].apply(list).reset_index()
+        
+        data_pd.to_csv(f'./result/{data}.csv')
 
     pass
