c7ae189d9c04c9f25977195446ede5a3e56b91d2
yangming.lin <yangming.lin@ehsn.com.tw>
2020-08-03 08:16:31 +0800

    add if condition for less alarm

--- exam1/algorithm.py
+++ exam1/algorithm.py
@@ -4,15 +4,25 @@ import numpy as np
 class Detector:
     def __init__(self):
         self.data_all = []
+        self.prd = []
 
     def fit_predict(self, ptr):
         self.data_all.append(ptr)
 
-        q1 = np.quantile(self.data_all[-100:], 0.25)
-        q3 = np.quantile(self.data_all[-100:], 0.7)
+        q1 = np.quantile(self.data_all[-50:], 0.25)
+        q3 = np.quantile(self.data_all[-50:], 0.75)
         iqr = q3 - q1
         
         lowBound = q1 - 1.5 * iqr
         upBound = q3 + 1.5 * iqr
-        pred = random.choices([0, 1], weights=[0.99, 0.01])[0]
-        return 0 if upBound >= ptr >= lowBound else 1
+
+        if len(self.prd) and self.prd[-1] == 1:
+            self.prd.append(0)
+            return 0
+        elif upBound >= ptr >= lowBound:
+            self.prd.append(0)
+            return 0
+        else :
+            self.prd.append(1)
+            return 1
+        # return 0 if upBound >= ptr >= lowBound else 1
